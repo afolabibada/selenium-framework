54b0d2d7b8f7f683e38b6b2681f7603dbcfe8b9b
afolabibada <afolabibada@gmail.com>
2024-10-23 15:17:39 +0100

    Order Mangementsteps

---
+++ src/test/java/com/testing/stepDefinition/OrderManagementSteps.java
@@ -0,0 +1,68 @@
+package com.testing.stepDefinition;
+
+import com.testing.cucumber.Hooks;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+public class OrderManagementsteps {
+
+    WebDriver driver = Hooks.driver;
+
+    @And("On my account page I click on What's New tab")
+    public void onMyAccountPageIClickOnWhatSNewTab() {
+    }
+
+    @And("I click on product link Echo compression Fit short")
+    public void iClickOnProductLinkEchoCompressionFitShort() {
+    }
+
+    @And("I click on product Size {int}")
+    public void iClickOnProductSize(int arg0) {
+    }
+
+    @And("I click on colour black")
+    public void iClickOnColourBlack() {
+    }
+
+    @And("I click on Add to cart button")
+    public void iClickOnAddToCartButton() {
+    }
+
+    @And("I click on the cart icon")
+    public void iClickOnTheCartIcon() {
+    }
+
+    @And("I click on the Proceed to checkout button")
+    public void iClickOnTheProceedToCheckoutButton() {
+    }
+
+    @And("On Checkout Page I enter {string} {string} {string} {string}")
+    public void onCheckoutPageIEnter(String arg0, String arg1, String arg2, String arg3) {
+    }
+
+    @And("I pick Flat rate")
+    public void iPickFlatRate() {
+    }
+
+    @And("I click on Next Button")
+    public void iClickOnNextButton() {
+    }
+
+    @When("I click on Place Order Button")
+    public void iClickOnPlaceOrderButton() {
+    }
+
+    @And("I click on My Account from profile drop down menu")
+    public void iClickOnMyAccountFromProfileDropDownMenu() {
+    }
+
+    @And("I click on My Orders")
+    public void iClickOnMyOrders() {
+    }
+
+    @Then("Order History should be displayed")
+    public void orderHistoryShouldBeDisplayed() {
+    }
+}
--- src/test/resources/Feature/OrderMangement.feature
+++ src/test/resources/Feature/OrderMangement.feature
@@ -1,2 +1,22 @@
 @OrderManagement
   Feature: Order Management
+    #Gherkin Steps
+  @CreateOrder
+  Scenario Outline: Have at least one order available to view history
+    Given I am on create an account page
+    And I enter "<FirstName>" "<LastName>" "<Email>" "<Password>" "<ConfirmPassword>"
+    And I click on create an account
+    And On my account page I click on What's New tab
+    And I click on product link Echo compression Fit short
+    And I click on product Size 28
+    And I click on colour black
+    And I click on Add to cart button
+    And I click on the cart icon
+    And I click on the Proceed to checkout button
+    And On Checkout Page I enter "<Address>" "<State>" "<PostalCode>" "<PhoneNumber>"
+    And I pick Flat rate
+    And I click on Next Button
+    When I click on Place Order Button
+    And I click on My Account from profile drop down menu
+    And I click on My Orders
+    Then Order History should be displayed
