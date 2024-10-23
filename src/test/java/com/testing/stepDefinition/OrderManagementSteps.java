e3c5b7495c27d4a72e6d6449db4e54abb9c96b6d
afolabibada <afolabibada@gmail.com>
2024-10-23 15:36:52 +0100

    Change OrderMangementSteps

--- src/test/java/com/testing/stepDefinition/OrderManagementSteps.java
+++ src/test/java/com/testing/stepDefinition/OrderManagementSteps.java
@@ -6,7 +6,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 
-public class OrderManagementsteps {
+public class OrderManagementSteps {
 
     WebDriver driver = Hooks.driver;
 
