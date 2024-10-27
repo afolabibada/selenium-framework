6fbf8d9e8d6ab7b7a54c6255f928b12a5ad77c6c
afolabibada <afolabibada@gmail.com>
2024-10-27 01:05:51 +0100

    Change OrderPagePO

---
+++ src/test/java/com/testing/pageObject/OrderPagePO.java
@@ -0,0 +1,14 @@
+package com.testing.pageObject;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class OrderPagePO {
+
+    //ElemEnt Locator
+
+    @FindBy(id = "firstname")
+    public static WebElement FirstName;
+
+
+}
